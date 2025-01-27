8fa4a9ea2fd927361c57f9d7fb72c7db12982de1
socalccna54 <socalccna@gmail.com>
2025-01-27 11:14:40 -0800

    Update unifi-server-install-script.sh
    Updated MongoDB public GPG key

--- unifi-server-install-script.sh
+++ unifi-server-install-script.sh
@@ -6,8 +6,8 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 # Install required packages
 sudo apt install -y gnupg ca-certificates apt-transport-https
 # Add MongoDB GPG key and repository
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ arch=amd64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 # Add Unifi GPG key and repository
 curl -fsSL https://dl.ui.com/unifi/unifi-repo.gpg | sudo gpg -o /usr/share/keyrings/unifi-repo.gpg --dearmor
 echo 'deb [ arch=amd64 signed-by=/usr/share/keyrings/unifi-repo.gpg ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/unifi.list
