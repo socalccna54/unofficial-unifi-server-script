768dcf789117773132db9a9fd0c5e51b6af09809
socalccna54 <socalccna@gmail.com>
2025-01-27 08:14:43 -0800

    Delete UnifServer-script.sh

--- UnifServer-script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-### UniFi Server Install Script ###
-### Credits go to eW4GJMqscYtbBkw9 from reddit
-# Update all package lists, upgrade all packages, and remove unecessary packages
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-# Install required packages
-sudo apt install -y gnupg ca-certificates apt-transport-https
